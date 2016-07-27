c9cc417beb91abb337b4558d66f1cf3bef6dbc9b
Sergey <santa-klaus88@ya.ru>
2016-07-27 20:24:51 +0300

    Initial Commit

---
+++ table-01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  table-01
+//
+//  Created by Serhii Serhiienko on 7/27/16.
+//  Copyright © 2016 Serhii Serhiienko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
