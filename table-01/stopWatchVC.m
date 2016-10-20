4a03c43a50f9834c2846d71fb547bd9b2bc24bec
Sergey <santa-klaus88@ya.ru>
2016-10-20 20:56:17 +0300

    Stopwatch comments cleared
    Deleted unnecessary comments

--- table-01/stopWatchVC.m
+++ table-01/stopWatchVC.m
@@ -25,7 +25,6 @@ typedef enum: NSUInteger {
     StopwatchStateReset
 } StopwatchStateType;
 
-//BOOL started = FALSE;
 StopwatchStateType currentWatchState = StopwatchStateReady;
 double startTime;
 double beforeStopTime = 0.0;
@@ -37,12 +36,10 @@ double lastCircleTime = 0.0;
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     circleList = [[NSMutableArray alloc] init];
-//    circleList = [NSMutableArray arrayWithCapacity:50];
 }
 
 
 - (IBAction)pushStartStopBtn:(id)sender {
-//    [self startStopCount];
     if (currentWatchState == StopwatchStateReady || currentWatchState == StopwatchStatePaused) {
         [self setStopwatchState:StopwatchStateStart];
     } else if (currentWatchState == StopwatchStateStarted) {
@@ -51,7 +48,6 @@ double lastCircleTime = 0.0;
 }
 
 - (IBAction)pushResetBtn:(id)sender {
-//    [self resetCount];
     if (currentWatchState == StopwatchStateStarted) {
         [self setStopwatchState:StopwatchStateCircle];
     } else if (currentWatchState == StopwatchStatePaused) {
@@ -88,71 +84,6 @@ double lastCircleTime = 0.0;
     }
     [self changeButtonsForState:state];
 }
-
-//- (void)startStopCount {
-//    if (currentWatchState == StopwatchStateReady || currentWatchState == StopwatchStatePaused) {
-//        currentWatchState = StopwatchStateStarted;
-//        [self startTimer];
-////        [self.startStopBtn setTitle:@"Pause" forState:UIControlStateNormal];
-////        [self.startStopBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
-////        
-////        self.resetBtn.enabled = YES;
-////        [self.resetBtn setTitle:@"Circle" forState:UIControlStateNormal];
-////        [self.resetBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-//        [self changeButtons];
-//        return;
-//    }
-//
-//    if (currentWatchState == StopwatchStateStarted) {
-//        double stopTime = [NSDate timeIntervalSinceReferenceDate];
-//        beforeStopTime += stopTime - startTime;
-//        beforeStopCircleTime += stopTime - lastCircleTime;
-//        [self stopTimer];
-//        
-////        [self.startStopBtn setTitle:@"Resume" forState:UIControlStateNormal];
-////        [self.startStopBtn setTitleColor:[UIColor greenColor] forState:UIControlStateNormal];
-////        
-////        [self.resetBtn setTitle:@"Reset" forState:UIControlStateNormal];
-////        [self.resetBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-//        currentWatchState = StopwatchStatePaused;
-//        [self changeButtons];
-//        return;
-//    }
-//}
-
-//- (void)resetCount {
-//    if (currentWatchState == StopwatchStateStarted) {
-////        NSLog(@"%@", self.circleTimeText.text);
-//        [circleList addObject:self.circleTimeText.text];
-////        NSLog(@"%@", circleList);
-//        
-//        lastCircleTime = [NSDate timeIntervalSinceReferenceDate];
-//        beforeStopCircleTime = 0.0;
-//    }
-//    
-//    if (currentWatchState == StopwatchStatePaused) {
-//    
-//        [self stopTimer];
-//        startTime = 0.0;
-//        beforeStopTime = 0.0;
-//        beforeStopCircleTime = 0.0;
-//        lastCircleTime = 0.0;
-//        [circleList removeAllObjects];
-////        [self.startStopBtn setTitle:@"Start" forState:UIControlStateNormal];
-////        [self.startStopBtn setTitleColor:[UIColor greenColor] forState:UIControlStateNormal];
-////        [self.resetBtn setTitle:@"Circle" forState:UIControlStateNormal];
-////        [self.resetBtn setTitleColor:[UIColor colorWithRed:85/255.0
-////                                                     green:85/255.0
-////                                                      blue:85/255.0
-////                                                     alpha:1.0] forState:UIControlStateNormal];
-////        self.resetBtn.enabled = NO;
-//        currentWatchState = StopwatchStateReady;
-//        [self changeButtons];
-//        [self.watchText setText:@"00:00.00"];
-//        [self.circleTimeText setText:@"00:00.00"];
-//    }
-//    [self.circleTable reloadData];
-//}
 
 - (void)changeButtonsForState:(StopwatchStateType)state {
     if (state == StopwatchStateStart) {
@@ -208,40 +139,11 @@ double lastCircleTime = 0.0;
     if (currentWatchState == StopwatchStateStarted) {
         double currentTime = [NSDate timeIntervalSinceReferenceDate];
         double totalSeconds = currentTime - startTime + beforeStopTime;
-//        double seconds = fmod(totalSeconds, 60.0);
-//        int minutes = (int)totalSeconds / 60;
         
         double circleTime = currentTime - lastCircleTime + beforeStopCircleTime;
-//        double circleSeconds = fmod(circleTime, 60.0);
-//        int circleMinutes = (int)circleTime / 60;
-        
-//        NSString *sec = [NSString stringWithFormat:@"%.2f", seconds];
-//        if (seconds < 10) {
-//            sec = [NSString stringWithFormat:@"0%.2f", seconds];
-//        }
-//        
-//        NSString *min = [NSString stringWithFormat:@"%i", minutes];
-//        if (minutes < 10) {
-//            min = [NSString stringWithFormat:@"0%i", minutes];
-//        }
-//        
-//        NSString *timeText = [NSString stringWithFormat:@"%@:%@", min, sec];
         
         NSString *timeText = [self createTimeFromSeconds:totalSeconds];
         [self.watchText setText:timeText];
-        
-        
-//        NSString *circleSec = [NSString stringWithFormat:@"%.2f", circleSeconds];
-//        if (circleSeconds < 10) {
-//            circleSec = [NSString stringWithFormat:@"0%.2f", circleSeconds];
-//        }
-//        
-//        NSString *circleMin = [NSString stringWithFormat:@"%i", circleMinutes];
-//        if (circleMinutes < 10) {
-//            circleMin = [NSString stringWithFormat:@"0%i", circleMinutes];
-//        }
-//        
-//        NSString *circleTimeText = [NSString stringWithFormat:@"%@:%@", circleMin, circleSec];
         
         NSString *circleTimeText = [self createTimeFromSeconds:circleTime];
         [self.circleTimeText setText:circleTimeText];
